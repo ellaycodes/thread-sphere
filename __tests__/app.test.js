f778c725dbd70c1b64975b159a0aac50e210d9be
Esther <11yekinie@gmail.com>
2023-11-21 16:49:58 +0000

    add tests for get comments by article id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,8 @@ const request = require("supertest");
 const app = require("../app");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
-const fs = require("fs");
-const endPoints = require('../endpoints.json');
+const endPoints = require("../endpoints.json");
+const jestS = require("jest-sorted");
 
 afterAll(() => {
   db.end();
@@ -65,15 +65,15 @@ describe("GET /api/articles/:article_id", () => {
       .get("/api/articles/1")
       .expect(200)
       .then(({ body }) => {
-          expect(body.article).toEqual({
-            author: expect.any(String),
-            title: expect.any(String),
-            article_id: 1,
-            body: expect.any(String),
-            topic: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number),
-            article_img_url: expect.any(String),
+        expect(body.article).toEqual({
+          author: expect.any(String),
+          title: expect.any(String),
+          article_id: 1,
+          body: expect.any(String),
+          topic: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
         });
       });
   });
@@ -94,3 +94,32 @@ describe("GET /api/articles/:article_id", () => {
       });
   });
 });
+
+// describe("GET /api/articles/:article_id/comments", () => {
+//   test("GET: 200 - responds with an array of comments for the given article_id", () => {
+//     return request(app)
+//       .get("/api/articles/:article_id/comments")
+//       .expect(200)
+//       .then(({ body }) => {
+//         expect(Array.isArray(body.comments)).toBe(true);
+//         body.comments.forEach((comment) => {
+//           expect(comment).toEqual({
+//             comment_id: expect.any(Number),
+//             votes: expect.any(String),
+//             created_at: expect.any(String),
+//             author: expect.any(String),
+//             body: expect.any(String),
+//             article_id: expect.any(Number),
+//           });
+//         });
+//       });
+//   });
+//   test("GET: 200 - Comments should be served with the most recent comments", () => {
+//     return request(app)
+//       .get("/api/articles/:article_id/comments")
+//       .expect(200)
+//       .then(({body}) => {
+//         expect(body.comments).toBeSortedBy('created_at', { descending: true})
+//       });
+//   });
+// });
