cb7253f3398d384a70a64c29b83cd6ebf66fcb15
Esther <11yekinie@gmail.com>
2023-11-21 11:54:14 +0000

    ignore commented code

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const app = require("../app");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const fs = require("fs");
+const endPoints = require('../endpoints.json');
 
 afterAll(() => {
   db.end();
@@ -20,6 +21,7 @@ describe("GET /api/topics", () => {
       .expect(200)
       .then(({ body }) => {
         expect(Array.isArray(body.topic)).toBe(true);
+        expect(body.topic.length).toBe(3);
         body.topic.forEach((topic) => {
           expect(topic).toEqual({
             slug: expect.any(String),
@@ -40,12 +42,10 @@ describe("GET /api/topics", () => {
 
 describe("GET /api", () => {
   test("GET: 200 - responds with an object describing all the available endpoints on your API ", () => {
-    const endPoints = JSON.parse(fs.readFileSync(`endpoints.json`));
     return request(app)
       .get("/api")
       .expect(200)
-      .then(({body}) => {
-        console.log(body);
+      .then(({ body }) => {
         expect(body).toEqual(endPoints);
       });
   });
@@ -54,7 +54,43 @@ describe("GET /api", () => {
       .get("/apo")
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe('Not Found')
+        expect(body.msg).toBe("Not Found");
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("GET: 200 - responds with an article object with the correct properties", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual({
+          author: expect.any(String),
+          title: expect.any(String),
+          article_id: expect.any(Number),
+          body: expect.any(String),
+          topic: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
+        });
+      });
+  });
+  test("GET: 404 - responds with an error message for incorrect or non-existent routes (1)", () => {
+    return request(app)
+      .get("/api/not-a-route")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not Found");
+      });
+  });
+  test("GET: 404 - responds with an error message for non-existent article_id (2)", () => {
+    return request(app)
+      .get("/api/articles/200")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not Found");
       });
   });
 });
