e931caac216ef2778c078f7fac83154ff75d9c96
Esther <11yekinie@gmail.com>
2023-11-21 16:25:18 +0000

    create an articles endpoint to select all articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,8 @@ const app = require("../app");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const fs = require("fs");
-const endPoints = require('../endpoints.json');
+const endPoints = require("../endpoints.json");
+const jestS = require("jest-sorted");
 
 afterAll(() => {
   db.end();
@@ -46,6 +47,7 @@ describe("GET /api", () => {
       .get("/api")
       .expect(200)
       .then(({ body }) => {
+        console.log(body);
         expect(body).toEqual(endPoints);
       });
   });
@@ -65,15 +67,15 @@ describe("GET /api/articles/:article_id", () => {
       .get("/api/articles/1")
       .expect(200)
       .then(({ body }) => {
-          expect(body.article).toEqual({
-            author: expect.any(String),
-            title: expect.any(String),
-            article_id: 1,
-            body: expect.any(String),
-            topic: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number),
-            article_img_url: expect.any(String),
+        expect(body.article).toEqual({
+          author: expect.any(String),
+          title: expect.any(String),
+          article_id: 1,
+          body: expect.any(String),
+          topic: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
         });
       });
   });
@@ -88,6 +90,75 @@ describe("GET /api/articles/:article_id", () => {
   test("GET: 404 - responds with an error message for non-existent article_id (2)", () => {
     return request(app)
       .get("/api/articles/200")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not Found");
+      });
+  });
+});
+
+describe("GET /api/articles", () => {
+  test("GET: 200 - responds with an articles array of article objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toHaveLength(5);
+        body.articles.forEach((article) => {
+          expect(article).toEqual({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(String),
+          });
+        });
+      });
+  });
+  test("GET: 200 - responds with comment_count which is the total count of all the comments with this article_id", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        function calculateExpectedCommentCount(id) {
+          if (id === 9 || id === 3 || id === 5) {
+            return '2';
+          } else if (id === 1) {
+            return '11';
+          } else if (id === 6) {
+            return '1';
+          }
+        }
+        body.articles.forEach((article) => {
+          const expectedCommentCount = calculateExpectedCommentCount(
+            article.article_id
+          );
+          expect(article.comment_count).toEqual(expectedCommentCount);
+        });
+      });
+  });
+  test("GET: 200 - articles should be sorted by date in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+  test("GET: 200 - there should not be a body property present on any of the article objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).not.toHaveProperty("body");
+      });
+  });
+  test("GET: 404 - responds with an error message for incorrect or non-existent routes", () => {
+    return request(app)
+      .get("/api/article")
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("Not Found");
--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,26 @@
 const db = require("../db/connection");
 
+exports.selectAllArticles = () => {
+  return db
+    .query(
+      `SELECT articles.author, articles.title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id) AS comment_count
+      FROM articles 
+    JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.author, 
+    articles.title, 
+    articles.article_id, 
+    topic, 
+    articles.created_at, 
+    articles.votes, 
+    article_img_url
+    ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      console.log(rows);
+      return rows;
+    });
+};
+
 exports.selectArticleById = (article_id) => {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
