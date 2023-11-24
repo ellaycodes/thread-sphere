7de44a390c5cb27006b171a821023081c2e5284a
Esther <11yekinie@gmail.com>
2023-11-24 13:14:16 +0000

    updates article id endpoint so that it includes comment count

--- errors.js
+++ errors.js
@@ -1,7 +1,7 @@
 exports.psqlErrors = (err, req, res, next) => {
   if (err.code === '23503') {
     res.status(404).send({ msg: "Article Not Found"});
-  } else if (err.code === '23502') {
+  } else if (err.code === '23502' || err.code === '22P02') {
     res.status(400).send({msg: "Bad Request"});
   } else {
     next(err);
--- models/articles.model.js
+++ models/articles.model.js
@@ -20,20 +20,19 @@ ORDER BY articles.created_at DESC;`
         return rows;
       });
   } else {
-    
-  const selectFromDB = `SELECT articles.author, articles.title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id) AS comment_count
+    const selectFromDB = `SELECT articles.author, articles.title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id) AS comment_count
   FROM articles`;
 
-  const joinDB = `LEFT JOIN comments ON articles.article_id = comments.article_id`;
+    const joinDB = `LEFT JOIN comments ON articles.article_id = comments.article_id`;
 
-  let whereDB;
-  if (topic && topic !== undefined && typeof topic === "string") {
-    whereDB = `WHERE topic = $1 `;
-  } else {
-    whereDB = ``;
-  }
+    let whereDB;
+    if (topic && topic !== undefined && typeof topic === "string") {
+      whereDB = `WHERE topic = $1 `;
+    } else {
+      whereDB = ``;
+    }
 
-  const groupByDB = `GROUP BY articles.author, 
+    const groupByDB = `GROUP BY articles.author, 
 articles.title, 
 articles.article_id, 
 topic, 
@@ -41,30 +40,41 @@ articles.created_at,
 articles.votes, 
 article_img_url`;
 
-  const orderByDB = `ORDER BY articles.created_at DESC`;
+    const orderByDB = `ORDER BY articles.created_at DESC`;
 
-  return db
-    .query(`${selectFromDB} ${joinDB} ${whereDB}${groupByDB} ${orderByDB};`, [
-      topic,
-    ])
-    .then(({ rows }) => {
-      return rows;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return db
+      .query(`${selectFromDB} ${joinDB} ${whereDB}${groupByDB} ${orderByDB};`, [
+        topic,
+      ])
+      .then(({ rows }) => {
+        return rows;
+      });
   }
 };
 
 exports.selectArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .query(
+      `SELECT articles.author, articles.title, articles.article_id, articles.body, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.author, 
+articles.title, 
+articles.article_id,
+articles.body, 
+topic, 
+articles.created_at, 
+articles.votes, 
+article_img_url`,
+      [article_id]
+    )
     .then(({ rows }) => {
       if (!rows.length) {
         return Promise.reject({ status: 404, msg: "Not Found" });
       }
       return rows;
-    });
+    })
 };
 
 exports.selectCommentsByArticleId = (id) => {
